5df71ea55a89a76509cebb14c71ff1ff62fd5a8a
vsupe92 <108114349+vsupe92@users.noreply.github.com>
2022-06-24 13:27:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          //Edit <code>src/App.js </code>#
           Hello Welcome to React-App Heruko Demo.
         </p>
         <a
